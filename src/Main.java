9c9a5a1c5034705216ba41b0cf6fc9d58a84bfc6
HP <mariem.jemaiel@esprit.tn>
2023-12-10 20:57:08 +0100

    first

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,6 @@ import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
-        //Instruction 2
         Employe e1 = new Employe(1,"1","1","1",1);
         Employe e2 = new Employe(2,"2","2","2",2);
         Employe e3 = new Employe(3,"3","3","3",3);
@@ -15,16 +14,12 @@ public class Main {
         affectations.ajouterEmployeDepartement(e2,d2);
         affectations.ajouterEmployeDepartement(e3,d3);
         affectations.ajouterEmployeDepartement(e1,d3);
-        //Instruction 3
         System.out.println("Instruction 3 :");
         System.out.println("Avant changement :");
         affectations.afficherEmployesEtDepartements();
         affectations.ajouterEmployeDepartement(e1,d1);
         System.out.println("Aprés changement :");
         affectations.afficherEmployesEtDepartements();
-        //On remarque que l'ajout de nouveau département d1 a écrasé l'ancien département d3 et le remplacé
-        System.out.println("------------------------------------------------------------");
-        //Instruction 4
         System.out.println("Instruction 4 :");
         Employe e4 = new Employe(4,"4","4","2",4);
         affectations.ajouterEmployeDepartement(e4,d2);
@@ -33,8 +28,6 @@ public class Main {
         affectations.supprimerEmploye(e4);
         System.out.println("Aprés suppression de e4 :");
         affectations.afficherEmployesEtDepartements();
-        System.out.println("------------------------------------------------------------");
-        //Instruction 5
         System.out.println("Instruction 5 :");
         Employe e5 = new Employe(5,"5","5","5",5);
         Departement d5 = new Departement(5,"5",50);
@@ -44,26 +37,16 @@ public class Main {
         affectations.supprimerEmployeEtDepartement(e5,d5);
         System.out.println("Aprés suppression de e5 qui travaille dans d5 :");
         affectations.afficherEmployesEtDepartements();
-        System.out.println("------------------------------------------------------------");
-        //Instruction 6
         System.out.println("Instruction 6 :");
         System.out.println("Liste employes :");
         affectations.afficherEmployes();
-        System.out.println("------------------------------------------------------------");
-        //Instruction 7
         System.out.println("Instruction 7 :");
         System.out.println("Liste departements :");
         affectations.afficherDepartements();
-        System.out.println("------------------------------------------------------------");
-        //Instruction 8
         System.out.println("Instruction 8 :");
         System.out.println("Est-ce que e1 fait partie de notre collection ? : "+affectations.rechercherEmploye(e1));
-        System.out.println("------------------------------------------------------------");
-        //Instruction 9
         System.out.println("Instruction 9 :");
         System.out.println("Est-ce que d5 fait partie de notre collection ? : "+affectations.rechercherDepartement(d5));
-        System.out.println("------------------------------------------------------------");
-        //Instruction 10
         System.out.println("Instruction 10 :");
         TreeMap<Employe,Departement> tree = affectations.trierMap();
         for (Map.Entry<Employe, Departement> t : tree.entrySet())
