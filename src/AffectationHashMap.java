ecfe37139a4a182a6004444977bea1ada2b727bb
HP <mariem.jemaiel@esprit.tn>
2023-12-10 21:11:19 +0100

    first

--- src/AffectationHashMap.java
+++ src/AffectationHashMap.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class AffectationHashMap {
+import java.util.*;
+
+public class AffectationHashMap {
+    private Map<Employe, Departement> affectations;
+
+    public AffectationHashMap() {
+        affectations = new HashMap<>();
+    }
+    public void ajouterEmployeDepartement(Employe e, Departement d){
+        affectations.put(e,d);
+    }
+    public void afficherEmployesEtDepartements(){
+        for (Map.Entry<Employe, Departement> affectation : affectations.entrySet())
+            System.out.println("Employe : "+affectation.getKey()+"  -->  Departement : "+affectation.getValue());
+    }
+    public void supprimerEmploye (Employe e){
+        affectations.remove(e);
+    }
+    public void supprimerEmployeEtDepartement (Employe e, Departement d){
+        for (Map.Entry<Employe, Departement> affectation : affectations.entrySet())
+            if (affectation.getKey().equals(e) && affectation.getValue().equals(d))
+                affectations.remove(e);
+    }
+    public void afficherEmployes(){
+        for (Employe employe : affectations.keySet())
+            System.out.println("- "+employe);
+    }
+    public void afficherDepartements(){
+        for (Departement departement : affectations.values())
+            System.out.println("- "+departement);
+    }
+    public boolean rechercherEmploye (Employe e){
+        return affectations.containsKey(e);
+    }
+    public boolean rechercherDepartement (Departement d){
+        return affectations.containsValue(d);
+    }
+    public TreeMap<Employe, Departement> trierMap(){
+        return new TreeMap<>(affectations);
+    }
 }
